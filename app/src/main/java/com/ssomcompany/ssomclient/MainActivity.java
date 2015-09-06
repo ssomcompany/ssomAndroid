4032f542c7e181b18240eb33173cf67b5257a4c7
kshgizmo <kshgizmo@gmail.com>
2015-09-06 19:24:59 +0900

    modify position of action bar

--- app/src/main/java/com/ssomcompany/ssomclient/MainActivity.java
+++ app/src/main/java/com/ssomcompany/ssomclient/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -13,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.ssomcompany.ssomclient.push.PushManageService;
 
