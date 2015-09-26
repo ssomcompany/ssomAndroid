c38052b2018f8f5f4de37492c5c0fb7a3876bddc
kshgizmo <kshgizmo@gmail.com>
2015-09-26 20:30:53 +0900

    add network config

---
+++ app/src/main/java/com/ssomcompany/ssomclient/network/NetworkConstant.java
@@ -0,0 +1,8 @@
+package com.ssomcompany.ssomclient.network;
+
+/**
+ * Created by kshgizmo on 2015. 9. 26..
+ */
+public class NetworkConstant {
+    public static final String host = "http://54.64.154.188/";
+}
