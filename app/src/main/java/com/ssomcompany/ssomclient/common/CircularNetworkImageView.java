0b85d6feecaf0c0caad9159fe1384616c8720e7a
kshgizmo <kshgizmo@gmail.com>
2015-10-30 21:27:10 +0900

    remove unused import

--- app/src/main/java/com/ssomcompany/ssomclient/common/CircularNetworkImageView.java
+++ app/src/main/java/com/ssomcompany/ssomclient/common/CircularNetworkImageView.java
@@ -2,14 +2,6 @@ package com.ssomcompany.ssomclient.common;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
-import android.graphics.RectF;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 
 import com.android.volley.toolbox.NetworkImageView;
