45ee5d32e7cefa2baa627ddc9d6892505fdb32ec
AaronMac <wolfjunghun@gmail.com>
2016-09-27 04:06:46 +0900

    [intro] intro 화면 3초 후 main으로 이동하게 변경

--- app/src/main/java/com/ssomcompany/ssomclient/activity/IntroActivity.java
+++ app/src/main/java/com/ssomcompany/ssomclient/activity/IntroActivity.java
@@ -1,6 +1,7 @@
 package com.ssomcompany.ssomclient.activity;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,24 +17,20 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-        ImageView btnStart= (ImageView) findViewById(R.id.btn_start);
-        btnStart.setOnClickListener(new View.OnClickListener() {
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
+            public void run() {
                 Intent i = new Intent();
                 i.setClass(getApplicationContext(), MainActivity.class);
                 i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(i);
             }
-        });
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        View decorView = getWindow().getDecorView();
-        // Hide the status bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
+        }, 3000);
     }
 }
