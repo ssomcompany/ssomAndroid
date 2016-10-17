1cb515caffb26f04107bae675476a1023821abbc
AaronMac <wolfjunghun@gmail.com>
2016-10-18 02:11:38 +0900

    [common] intro activity 구현, 버전에 따른 앱 사용금지 적용

--- app/src/main/java/com/ssomcompany/ssomclient/activity/IntroActivity.java
+++ app/src/main/java/com/ssomcompany/ssomclient/activity/IntroActivity.java
@@ -1,17 +1,22 @@
 package com.ssomcompany.ssomclient.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
 import android.view.View;
-import android.widget.ImageView;
 
 import com.ssomcompany.ssomclient.R;
+import com.ssomcompany.ssomclient.common.UiUtils;
+import com.ssomcompany.ssomclient.network.APICaller;
+import com.ssomcompany.ssomclient.network.NetworkManager;
+import com.ssomcompany.ssomclient.network.api.GetApplicationVersion;
+import com.ssomcompany.ssomclient.network.model.SsomResponse;
+import com.ssomcompany.ssomclient.widget.dialog.CommonDialog;
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +28,49 @@ public class IntroActivity extends AppCompatActivity {
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
 
-        new Handler().postDelayed(new Runnable() {
+        APICaller.getApplicationVersion(new NetworkManager.NetworkListener<SsomResponse<GetApplicationVersion.Response>>() {
             @Override
-            public void run() {
-                Intent i = new Intent();
-                i.setClass(getApplicationContext(), MainActivity.class);
-                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(i);
+            public void onResponse(SsomResponse<GetApplicationVersion.Response> response) {
+                if(response.isSuccess()) {
+                    String appVersion = response.getData().getVersion();
+                    appVersion = appVersion.replace(".", "");
+                    try {
+                        if(getPackageManager().getPackageInfo(getPackageName(), 0).versionCode < Integer.parseInt(appVersion)) {
+                            UiUtils.makeCommonDialog(IntroActivity.this, CommonDialog.DIALOG_STYLE_ALERT_BUTTON, R.string.dialog_notice, 0,
+                                    R.string.version_update_needed, R.style.ssom_font_16_custom_666666,
+                                    R.string.update, R.string.finish,
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            // google market 으로 이동
+                                            Intent marketIntent = new Intent(Intent.ACTION_VIEW);
+                                            marketIntent.setData(Uri.parse("market://details?id=" + getPackageName()));
+                                            startActivity(marketIntent);
+                                            finish();
+                                        }
+                                    }, new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            UiUtils.makeToastMessage(getApplicationContext(), "업데이트 진행 후 사용해 주시기 바랍니다 =)");
+                                            finish();
+                                        }
+                                    });
+                        } else {
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Intent i = new Intent();
+                                    i.setClass(getApplicationContext(), MainActivity.class);
+                                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(i);
+                                }
+                            }, 3000);
+                        }
+                    } catch(PackageManager.NameNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        }, 3000);
+        });
     }
 }
